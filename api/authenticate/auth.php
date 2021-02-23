62c2359b7482fdbb2b9f1dbccae1257d3ef4c1a0
ncarthy <neil@shandonsoft.co.uk>
2021-02-23 16:31:09 +0000

    Set Allow-Origin for Node development

--- api/authenticate/auth.php
+++ api/authenticate/auth.php
@@ -1,6 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Origin: http://localhost:4200");
+//header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
