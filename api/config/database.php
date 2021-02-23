301f6c18dc1578c1715b03f2e5454be1e8f2b23e
ncarthy <neil@shandonsoft.co.uk>
2021-02-23 09:32:23 +0000

    Ensure PDO uses true prepared statements to avoid SQL injection

--- api/config/database.php
+++ api/config/database.php
@@ -15,7 +15,12 @@ class Database{
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name
                 , $this->username, $this->password);
-            $this->conn->exec("set names utf8");            
+            $this->conn->exec("set names utf8");    
+
+            // From https://stackoverflow.com/a/60496/6941165
+            $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+                
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
