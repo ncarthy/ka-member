2e16991dd57a98e8cb5d6fa2fd807b61ae6cba42
ncarthy <neil@shandonsoft.co.uk>
2021-03-14 01:25:44 +0000

    comment out old regex's

--- ka-member-app/src/app/address-search/search-box.component.ts
+++ ka-member-app/src/app/address-search/search-box.component.ts
@@ -37,13 +37,13 @@ import { regExpEscape } from '@ng-bootstrap/ng-bootstrap/util/util';
     ngOnInit(): void {
 
       // From https://stackoverflow.com/a/51885364/6941165
-      const postcode = new RegExp("^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$", "i");      //'i' = case insensitive
+      /*const postcode = new RegExp("^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$", "i");      //'i' = case insensitive
       const postcode1 = new RegExp("^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$");
       const postcode2 = new RegExp("^([A-Z][A-HJ-Y]?[0-9][A-Z0-9]? ?[0-9][A-Z]{2}|GIR ?0A{2})$", "i");
-      const postcode3 = new RegExp("^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$", "i");
+      const postcode3 = new RegExp("^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$", "i"); // didn't work*/
       const postcode4 = new RegExp("^([A-Z][A-HJ-Y]?[0-9][A-Z0-9]? ?[0-9][A-Z]{2})$", "i");
 
-      console.log("Try 1", postcode.test("sw7 1jy"));
+      /*console.log("Try 1", postcode.test("sw7 1jy"));
       console.log("Try 2", postcode.test("SW7 1JY"));
       console.log("Try 3", postcode.test("SW71JY"));
       const test = new RegExp('SW7',"i");
@@ -53,7 +53,7 @@ import { regExpEscape } from '@ng-bootstrap/ng-bootstrap/util/util';
       console.log("Try 7", postcode1.test("sw7 1JY"));
       console.log("Try 8", postcode2.test("sw7 1JY"));
       console.log("Try 9", postcode3.test("sw7 1JY"));
-      console.log("Try 10", postcode4.test("sw7 1JY"));
+      console.log("Try 10", postcode4.test("sw7 1JY"));*/
 
         // convert the `keyup` event into an observable stream
         fromEvent(this.el.nativeElement, 'keyup').pipe(
