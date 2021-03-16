0f5ffb4b5425567218c1469ebc2c7ed294544bf6
ncarthy <neil@shandonsoft.co.uk>
2021-03-16 14:27:11 +0000

    make member list clickable

--- ka-member-app/src/app/members/layout.component.ts
+++ ka-member-app/src/app/members/layout.component.ts
@@ -1,16 +1,5 @@
 ﻿import { Component } from '@angular/core';
 
-import { AuthenticationService } from '@app/_services';
-import { Role, User} from '@app/_models';
 
 @Component({ templateUrl: 'layout.component.html' })
-export class LayoutComponent { 
-    user! : User;    
-
-    constructor(
-        private authenticationService: AuthenticationService
-    ) {
-        this.user = authenticationService.userValue;
-    }
-
-}
+export class LayoutComponent {}
--- ka-member-app/src/app/members/list.component.ts
+++ ka-member-app/src/app/members/list.component.ts
@@ -1,4 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
 import { first, take } from 'rxjs/operators';
 import { MemberService, MemberSearchService,AuthenticationService } from '@app/_services';
 import { Member,MemberSearchResult, User } from '@app/_models';
@@ -9,7 +12,8 @@ export class ListComponent implements OnInit {
     members!: MemberSearchResult[];
     user!: User;
 
-    constructor(private memberService: MemberService,
+    constructor(private router: Router,
+        private memberService: MemberService,
         private memberSearchService: MemberSearchService,
         private authenticationService: AuthenticationService) {
             this.user = authenticationService.userValue;
@@ -23,5 +27,9 @@ export class ListComponent implements OnInit {
 
     memberWasDeleted(member: MemberSearchResult): void {
         this.members = this.members.filter(x => x.id !== member.id);
+    }
+
+    memberSelected(member: MemberSearchResult): void {
+        this.router.navigate([`members/edit/${member.id}`]);
     }
 }
