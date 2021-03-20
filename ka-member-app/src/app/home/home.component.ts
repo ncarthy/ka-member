e7d1bb7926c916d8ffee851b85b6353819b2bb9b
ncarthy <neil@shandonsoft.co.uk>
2021-03-20 22:01:08 +0000

    Improvements to Home summary table

--- ka-member-app/src/app/home/home.component.ts
+++ ka-member-app/src/app/home/home.component.ts
@@ -15,7 +15,6 @@ export class HomeComponent implements OnInit{
     membersByType!: MemberCount[];
     total!: number;                         // Estimated number of members, adjusted by contribution
     count!: number;                         // Actual number of members
-    showCalculations: boolean = true;
 
     constructor(
         private userService: UserService,
