c9b3460ec7dd8aa3cd05f8e55934172c7ede21c7
ncarthy <neil@shandonsoft.co.uk>
2021-03-20 13:06:34 +0000

    Working on table responsiveness

--- ka-member-app/src/app/members/list/list.component.ts
+++ ka-member-app/src/app/members/list/list.component.ts
@@ -1,8 +1,8 @@
 ﻿import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
-
-import { first, take } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { first, debounceTime, map,startWith } from 'rxjs/operators';
 import { MemberSearchService,AuthenticationService } from '@app/_services';
 import { MemberSearchResult, User } from '@app/_models';
 
@@ -11,6 +11,7 @@ export class ListComponent implements OnInit {
     members!: MemberSearchResult[];
     user!: User;
     loading: boolean = false;
+    isScreenLarge$!: Observable<boolean>;
 
     constructor(private router: Router,
         private memberSearchService: MemberSearchService,
@@ -22,6 +23,16 @@ export class ListComponent implements OnInit {
         this.memberSearchService.search('')
             .pipe(first()) // TODO: this doesn't work
             .subscribe(members => this.members = members);
+
+        // Checks if screen size is less than 1024 pixels
+        const checkScreenSize = () => {console.log(document.body.offsetWidth); return document.body.offsetWidth >= 768;};
+        
+        // Create observable from window resize event throttled so only fires every 500ms
+        const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(500)).pipe(map(checkScreenSize));
+        
+        // Start off with the initial value use the isScreenSmall$ | async in the
+        // view to get both the original value and the new value after resize.
+        this.isScreenLarge$ = screenSizeChanged$.pipe(startWith(checkScreenSize()))
     }
 
     memberWasDeleted(member: MemberSearchResult): void {
