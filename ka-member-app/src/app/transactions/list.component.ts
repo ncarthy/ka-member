bb0c15d3520c22895eaf843b607605cca8b516d4
ncarthy <neil@shandonsoft.co.uk>
2021-03-24 17:38:33 +0000

    Change members routerlinks to use full path

--- ka-member-app/src/app/transactions/list.component.ts
+++ ka-member-app/src/app/transactions/list.component.ts
@@ -1,14 +1,23 @@
 ﻿import { Component, OnInit } from '@angular/core';
 
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  tap,
+  map,
+} from 'rxjs/operators';
+
 import { AuthenticationService } from '@app/_services';
-import { MemberFilter, MemberSearchResult, User, YesNoAny } from '@app/_models';
+import { MemberFilter, Transaction, User, YesNoAny } from '@app/_models';
 
 @Component({ templateUrl: 'list.component.html' })
 export class TransactionListComponent implements OnInit {
-  members!: MemberSearchResult[];
+  transactions!: Observable<Transaction[]>;
   user!: User;
   loading: boolean = false;
-  filter!: MemberFilter;
 
   constructor(
     private authenticationService: AuthenticationService
@@ -18,38 +27,7 @@ export class TransactionListComponent implements OnInit {
 
   ngOnInit() {
 
-    // Checks if screen size is less than 768 pixels
-    // Is used to show/hide table columns
-    const checkScreenSize = () => {
-      return document.body.offsetWidth <= 768;
-    };
   }
 
-  /* remove member from visible list */
-  memberWasDeleted(member: MemberSearchResult): void {
-    this.members = this.members.filter((x) => x.id !== member.id);
-  }
 
-  memberWasUpdated(member: MemberSearchResult): void {
-    var index = this.members.indexOf(member);
-    if (index !== -1) {
-      this.members[index] = member;
-  }
-  }
-
-  memberSelected(member: MemberSearchResult): void {
-    
-  }
-
-  membersUpdated(members: MemberSearchResult[]) {
-    this.members = members;
-  }
-
-  membersFilterUpdated(filter: MemberFilter[]) {
-    this.filter = filter;
-  }
-
-  filterIsLoading(value: boolean) {
-    this.loading = value;
-  }
 }
