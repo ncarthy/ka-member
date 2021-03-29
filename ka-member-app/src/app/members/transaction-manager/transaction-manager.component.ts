efa52cd1b06e2f43f001c6857d58706b91e0e91a
ncarthy <neil@shandonsoft.co.uk>
2021-03-29 23:48:40 +0100

    Move Transactions to new component

--- ka-member-app/src/app/members/transaction-manager/transaction-manager.component.ts
+++ ka-member-app/src/app/members/transaction-manager/transaction-manager.component.ts
@@ -1,14 +1,87 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import {
+  Member,
+  MembershipStatus,
+  Transaction,
+  User,
+} from '@app/_models';
+import {
+  AuthenticationService,
+  MemberNameService,
+  MemberService,
+  TransactionService,
+} from '@app/_services';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'transaction-manager',
-  templateUrl: './transaction-manager.component.html'
+  templateUrl: './transaction-manager.component.html',
 })
 export class TransactionManagerComponent implements OnInit {
+  loading: boolean = false;
+  member?: Member;
+  memberName?: string;
+  statuses!: MembershipStatus[];
+  transactions?: Transaction[];
+  transactionToEdit?: Transaction;
+  user: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private memberService: MemberService,
+    private transactionService: TransactionService,
+    private memberNameService: MemberNameService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.user = this.authenticationService.userValue;
   }
 
+  ngOnInit(): void {
+    this.loading = true;
+
+    this.memberService
+      .getById(this.route.snapshot.params['idmember'])
+      .pipe(
+        switchMap((m: Member) => {
+          this.member = m;
+          return this.transactionService.getByMember(m.id);
+        }),
+        switchMap((txs: Transaction[]) => {
+          this.transactions = txs;
+          return this.memberNameService.getNamesStringForMember(
+            this.member!.id
+          );
+        })
+      )
+      .subscribe((mn: string) => {
+        this.memberName = mn;
+        this.loading = false;
+      });
+  }
+
+  goBack() {
+    this.location.back();
+    return false; // don't propagate event
+  }
+
+  /** Reload transacitons table and add/edit transaction card */
+  onReloadRequested(t: Transaction) {
+    if (this.member) {
+      this.loading = true;
+      this.transactionService
+        .getByMember(this.member.id)
+        .subscribe((txs: Transaction[]) => {
+          this.transactions = txs;
+          this.loading = false;
+        });
+    }
+  }
+
+  onEditRequested(tx: Transaction) {
+    this.transactionToEdit = tx;
+  }
 }
