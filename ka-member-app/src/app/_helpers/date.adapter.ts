7fdcd219d8830d9b4d430077e57e9dd80c774646
ncarthy <neil.carthy42@gmail.com>
2024-05-20 00:37:47 +0100

    Add comments

--- ka-member-app/src/app/_helpers/date.adapter.ts
+++ ka-member-app/src/app/_helpers/date.adapter.ts
@@ -1,10 +1,17 @@
-// Code is from https://stackoverflow.com/a/47945155/6941165
-// Further information at https://ng-bootstrap.github.io/#/components/datepicker/overview#date-model
-// Example at https://ng-bootstrap.github.io/#/components/datepicker/examples#adapter
-
 import { Injectable } from '@angular/core';
 import { NgbDateAdapter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
+/**
+ *  Adapter class for angular powered bootstrap ngbDatepicker to convert stardard
+ *  date format (aka NgbDateStruct) to/from MySQL 'YYYY-MM-DD'.
+ *
+ *  Note: There is a separate CustomDateParserFormatter class that handles how the
+ *  date is represented on the screen in the input box atop the DatePicker
+ *
+ *  Code is from {@link https://stackoverflow.com/a/47945155/6941165}
+ *  Further information at {@link https://ng-bootstrap.github.io/#/components/datepicker/overview#date-model}
+ *  Example at {@linkhttps://ng-bootstrap.github.io/#/components/datepicker/examples#adapter}
+ */
 @Injectable()
 export class NgbUTCStringAdapter extends NgbDateAdapter<string> {
   fromModel(date: string): NgbDateStruct | null {
--- ka-member-app/src/app/_helpers/must-match.validator.ts
+++ ka-member-app/src/app/_helpers/must-match.validator.ts
@@ -1,6 +1,12 @@
 import { AbstractControl, FormGroup } from '@angular/forms';
 
-// custom validator to check that two fields match
+/**
+ * Custom validator to check that two control fields match
+ *
+ * @param controlName Name of first control to check
+ * @param matchingControlName Name of second control to check
+ * @returns If no match then return null, otherwise validate controls and exit
+ */
 export function MustMatch(controlName: string, matchingControlName: string) {
   return (group: AbstractControl) => {
     const formGroup = <FormGroup>group;
