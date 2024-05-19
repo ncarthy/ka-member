765db16d89793f88fa36f73e1194642405f11751
ncarthy <neil.carthy42@gmail.com>
2024-05-19 22:01:28 +0100

    Use cookie path

--- api/models/jwt.php
+++ api/models/jwt.php
@@ -352,7 +352,7 @@ class JWTWrapper{
         $token = $this->getToken($user_with_token['id'], $refreshJti, $now, $tokenExpiry);
 
         setcookie($this->cookiename, $token, $tokenExpiry->getTimestamp()
-            , \Core\Config::read('token.cookiepath'), $cookieDomain, $this->cookiesecure, true); // 'true' = HttpOnly
+            , $this->cookiepath, $cookieDomain, $this->cookiesecure, true); // 'true' = HttpOnly
 
         return $this->usertoken->store($user_with_token['id'], $user_with_token['accessJti'], $refreshJti, 
                     true, $tokenExpiry->format("Y-m-d H:i:s")); // 'true' = isValid
